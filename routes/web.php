9f2182c4d2a6a18e370f0b03f84643a14c6f754b
jocrock <jocrock.cga@gmail.com>
2020-05-13 14:52:59 -0400

    Se agrego la entidad Producto y todas las funcionalidades en el controlador y la vista mostrar, editar, eliminar

--- routes/web.php
+++ routes/web.php
@@ -27,51 +27,51 @@ Route::middleware(['auth'])->group(function(){
 
     // crear
     Route::post('roles/store','RoleController@store')->name('roles.store')
-        ->middleware('permssion:roles.create');
+        ->middleware('can:roles.create');
 
     //visualizar el sitado
     Route::get('roles','RoleController@index')->name('roles.index')
-        ->middleware('permssion:roles.index');
+        ->middleware('can:roles.index');
     // ver el formulario de creacion
     Route::get('roles/create','RoleController@create')->name('roles.create')
-        ->middleware('permssion:roles.create');
+        ->middleware('can:roles.create');
     // actualizar
     Route::put('roles/{role}','RoleController@update')->name('roles.update')
-        ->middleware('permssion:roles.edit');
+        ->middleware('can:roles.edit');
     //ver el detalle
     Route::get('roles/{role}','RoleController@show')->name('roles.show')
-        ->middleware('permssion:roles.show');
+        ->middleware('can:roles.show');
     // eliminar
     Route::delete('roles/{role}','RoleController@destroy')->name('roles.destroy')
-        ->middleware('permssion:roles.destroy');
+        ->middleware('can:roles.destroy');
     // ver el formulario de edicion
     Route::get('roles/{role}/edit','RoleController@edit')->name('roles.edit')
-        ->middleware('permssion:roles.edit');
+        ->middleware('can:roles.edit');
 
     //PRODUCTOS
 
     // crear
     Route::post('products/store','ProductController@store')->name('products.store')
-        ->middleware('permssion:products.create');
+        ->middleware('can:products.create');
 
     //visualizar el sitado
     Route::get('products','ProductController@index')->name('products.index')
-        ->middleware('permssion:products.index');
+        ->middleware('can:products.index');
     // ver el formulario de creacion
     Route::get('products/create','ProductController@create')->name('products.create')
-        ->middleware('permssion:products.create');
+        ->middleware('can:products.create');
     // actualizar
-    Route::put('products/{role}','ProductController@update')->name('products.update')
-        ->middleware('permssion:products.edit');
+    Route::put('products/{product}','ProductController@update')->name('products.update')
+        ->middleware('can:products.edit');
     //ver el detalle
-    Route::get('products/{role}','ProductController@show')->name('products.show')
-        ->middleware('permssion:products.show');
+    Route::get('products/{product}','ProductController@show')->name('products.show')
+        ->middleware('can:products.show');
     // eliminar
-    Route::delete('products/{role}','ProductController@destroy')->name('products.destroy')
-        ->middleware('permssion:products.destroy');
+    Route::delete('products/{product}','ProductController@destroy')->name('products.destroy')
+        ->middleware('can:products.destroy');
     // ver el formulario de edicion
-    Route::get('products/{role}/edit','ProductController@edit')->name('products.edit')
-        ->middleware('permssion:products.edit');
+    Route::get('products/{product}/edit','ProductController@edit')->name('products.edit')
+        ->middleware('can:products.edit');
 
 
     //USUARIOS
@@ -79,18 +79,18 @@ Route::middleware(['auth'])->group(function(){
  
     //visualizar el sitado
     Route::get('users','UserController@index')->name('users.index')
-        ->middleware('permssion:users.index');
+        ->middleware('can:users.index');
 
     // actualizar
     Route::put('users/{role}','UserController@update')->name('users.update')
-        ->middleware('permssion:users.edit');
+        ->middleware('can:users.edit');
     //ver el detalle
     Route::get('users/{role}','UserController@show')->name('users.show')
-        ->middleware('permssion:users.show');
+        ->middleware('can:users.show');
     // eliminar
     Route::delete('users/{role}','UserController@destroy')->name('users.destroy')
-        ->middleware('permssion:users.destroy');
+        ->middleware('can:users.destroy');
     // ver el formulario de edicion
     Route::get('users/{role}/edit','UserController@edit')->name('users.edit')
-        ->middleware('permssion:users.edit');
+        ->middleware('can:users.edit');
 });
